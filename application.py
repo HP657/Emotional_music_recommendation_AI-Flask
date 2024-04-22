446f93e05b9603f8ec34ed8dace57014a867e1a9
고기맛호빵 <junlee5070@gmail.com>
2024-04-22 13:37:29 +0000

    감정을 통해 뽑은 노래로 랜덤하게 노래를 반환해주는 api로 변경

--- application.py
+++ application.py
@@ -5,9 +5,13 @@ from sklearn.svm import SVC
 from joblib import load
 from flask_cors import CORS
 import sys
+import requests
+import os
+import random  # 랜덤 모듈 추가
 
 app = Flask(__name__)
 CORS(app)
+
 # Okt 형태소 분석기 초기화
 okt = Okt()
 
@@ -15,8 +19,10 @@ okt = Okt()
 model = Word2Vec.load("modeling/word2vec_model.bin")
 
 # SVM 모델 불러오기
-svm_model = SVC(kernel='linear')
 svm_model = load("modeling/svm_model.joblib")
+
+# Last.fm API key
+last_fm_api_key = os.getenv("song_api")
 
 # 형태소 분석 함수 정의
 def tokenize(text):
@@ -29,26 +35,40 @@ def get_embedding(model, tokens):
         return sum(embeddings) / len(embeddings)
     else:
         return []
-    
-@app.route("/", methods=['GET'])
-def main():
-    return 'hello'
 
-@app.route('/classify', methods=['POST'])
-def classify_emotion():
+# Last.fm에서 감정에 맞는 곡을 검색하는 함수
+def search_tracks_by_emotion(emotion):
+    url = f"http://ws.audioscrobbler.com/2.0/?method=track.search&track={emotion}&api_key={last_fm_api_key}&format=json"
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        tracks = data['results']['trackmatches']['track']
+        if tracks:
+            random_track = random.choice(tracks)  # 랜덤한 트랙 선택
+            return random_track['name'], random_track['artist']
+    return None, None
+
+@app.route('/recommend_song', methods=['POST'])
+def recommend_song():
     data = request.json
     sentence = data['sentence']
-
+    
     # 형태소로 분할
     tokenized_sentence = tokenize(sentence)
-
+    
     # 문장을 Word2Vec 임베딩으로 변환
     embedding = get_embedding(model, tokenized_sentence)
-
+    
     # 감정 분류
     emotion = svm_model.predict([embedding])[0]
-
-    return jsonify({"emotion": emotion})
+    print(emotion)
+    # 감정에 따른 곡 추천
+    track_name, artist = search_tracks_by_emotion(emotion)
+    print(track_name, artist)
+    if track_name and artist:
+        return jsonify({"emotion": emotion, "recommended_track": f"{track_name} by {artist}"})
+    else:
+        return jsonify({"emotion": emotion, "recommended_track": "No track found for the specified emotion"})
 
 if __name__ == '__main__':
-    app.run(port=int(sys.argv[1]),host="0.0.0.0",debug=True)
+    app.run(port=int(sys.argv[1]), host="0.0.0.0", debug=True)
